ea4b1956337c9d2a31f62298d1cfa14b4f0d0fa2
Arian Harveland <130916492+arianvbb@users.noreply.github.com>
2026-01-27 17:03:30 +0100

    Update Fonts.py

--- Fonts.py
+++ Fonts.py
@@ -1,1 +1,27 @@
+title_font = tkFont.Font(
+    family = "Arial",
+    size = 40, 
+    weight = "bold"
+)
 
+text_font = tkFont.Font(
+    family = "Arial",
+    size = 30,
+)
+
+button_font = tkFont.Font(
+    family = "Arial",
+    size = 60
+)
+
+countdown_font = tkFont.Font(
+    family = "Arial",
+    size = 60,
+    weight = "bold"
+)
+
+countdown_text_font = tkFont.Font(
+    family = "Arial",
+    size = 40,
+    weight = "bold"
+)
